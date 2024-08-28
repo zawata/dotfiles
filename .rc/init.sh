250731ff1101b29a425687a5ccf0205108f1ae07
John Alden <john.alden@gitkraken.com>
2024-08-28 10:28:21 -0700

    more minor updates

--- .rc/init.sh
+++ .rc/init.sh
@@ -4,9 +4,16 @@
 # Environment Initialization
 #
 
-# packages I need
-sudo pamac install fortune
-sudo pamac build visual-studio-code-bin
+## Package installation
+
+# official packagaes
+sudo pamac install alacritty firefox flameshot fortune-mod noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra
+
+# tools/utilities
+sudo pamac install docker docker-compose dog feh nmap gnu-netcat htop
+
+# AUR packages
+sudo pamac build 1password awsvpnclient etcher-cli-bin httpie-desktop-bin via-bin visual-studio-code-bin
 
 # install nvm
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
@@ -16,10 +23,10 @@ curl https://sh.rustup.rs -sSf | sh
 rustup update
 
 # install vcpkg
-cd /opt/
-mkdir vcpkg
-sudo chown $USER vcpkg
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-./bootstrap-vcpkg.sh -disableMetrics
+# cd /opt/
+# mkdir vcpkg
+# sudo chown $USER vcpkg
+# git clone https://github.com/Microsoft/vcpkg.git
+# cd vcpkg
+# ./bootstrap-vcpkg.sh -disableMetrics
 
--- .rc/zsh_scripts/system/core.sh
+++ .rc/zsh_scripts/system/core.sh
@@ -4,12 +4,12 @@
 # SSH
 #===============================================================================
 
-# setup auth sock
-export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
+# # setup auth sock
+# export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 
-#add all ssh keys
-add-ssh-keys() {
-  ssh-add $HOME/.ssh/*.id_rsa &> /dev/null
-  ssh-add $HOME/.ssh/*.id_ed25519 &> /dev/null
-}
-add-ssh-keys
+# #add all ssh keys
+# add-ssh-keys() {
+#   ssh-add $HOME/.ssh/*.id_rsa &> /dev/null
+#   ssh-add $HOME/.ssh/*.id_ed25519 &> /dev/null
+# }
+# add-ssh-keys
